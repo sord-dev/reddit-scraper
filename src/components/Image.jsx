5c7be5d2b4e0d400056954c333838216133f141d
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-06-26 09:19:59 +0100

    added modal component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,10 +1,60 @@
-import { Image} from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Tag,
+  TagLabel,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const CustomImage = (props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-      <a href={props.src}>
+    <>
+      <Box onClick={onOpen}>
         <Image alt={props.title} src={props.thumbnail} />
-      </a>
+      </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{props.title}</ModalHeader>
+          <ModalCloseButton />
+
+          <Image alt={props.title} src={props.thumbnail} />
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button as="a" href={props.src} variant="ghost">
+              To Reddit Post
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
